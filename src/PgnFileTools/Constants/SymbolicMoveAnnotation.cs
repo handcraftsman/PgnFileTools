1a45a2bf6caca3d00a70bb05c3f0b6080f879e9e
Clinton Sheppard <sheppard@cs.unm.edu>
2014-06-21 17:37:18 -0500

    GameInfo parser - handle move annotation '!?'

--- src/PgnFileTools/Constants/SymbolicMoveAnnotation.cs
+++ src/PgnFileTools/Constants/SymbolicMoveAnnotation.cs
@@ -7,6 +7,7 @@ namespace PgnFileTools
         public static readonly SymbolicMoveAnnotation GoodMove = new SymbolicMoveAnnotation("!", 1);
         public static readonly SymbolicMoveAnnotation PoorMove = new SymbolicMoveAnnotation("?", 2);
         public static readonly SymbolicMoveAnnotation QuestionableMove = new SymbolicMoveAnnotation("?!", 6);
+        public static readonly SymbolicMoveAnnotation SpeculativeMove = new SymbolicMoveAnnotation("!?", 5);
         public static readonly SymbolicMoveAnnotation VeryGoodMove = new SymbolicMoveAnnotation("!!", 3);
         public static readonly SymbolicMoveAnnotation VeryPoorMove = new SymbolicMoveAnnotation("??", 4);
 
--- src/PgnFileToolsTests/GameInfoParserTests.cs
+++ src/PgnFileToolsTests/GameInfoParserTests.cs
@@ -60,6 +60,19 @@ namespace PgnFileToolsTests
         }
 
         [Test]
+        public void Given_a_SPECULATIVE_MOVE_symbolic_annotation_after_a_move_text__should_add_the_annotation_to_the_Move_information()
+        {
+            const string input = "[Result \"1-0\"]\n1.a4!? 1-0\n";
+            var result = _parser.Parse(CreateStream(input));
+            result.Headers.Count.ShouldBeEqualTo(1);
+            result.Headers["Result"].ShouldBeEqualTo("1-0");
+            result.HasError.ShouldBeFalse();
+            result.Moves.Count.ShouldBeEqualTo(1);
+            result.Moves[0].ToAlgebraicString().ShouldBeEqualTo("a4");
+            result.Moves[0].Annotation.ShouldBeEqualTo(SymbolicMoveAnnotation.SpeculativeMove.Id);
+        }
+
+        [Test]
         public void Given_a_VERY_GOOD_MOVE_symbolic_annotation_after_a_move_text__should_add_the_annotation_to_the_Move_information()
         {
             const string input = "[Result \"1-0\"]\n1.a4!! 1-0\n";
